7f83af60c25bdd00b91de164cb29ee9360f256e8
Gustavo  Coutinho <gustavocoutinho@Gustavos-MacBook-Pro.local>
2018-01-17 12:49:28 -0500

    your account page, create playlist page, new css and new routes

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -3,9 +3,15 @@ const router = express.Router();
 const request = require('request');
 const config = require('../app/models/config');
 
+
+/*=============================================
+=            Routes for users
+=============================================*/
+
 router.get('/', (req, res, next) => {
     return res.render('index');
 });
+
 
 router.get('/register', (req, res, next) => {
     return res.render('register');
@@ -15,9 +21,24 @@ router.get('/login', (req, res, next) => {
     return res.render('login');
 });
 
+
 router.get('/account', (req, res, next) => {
     return res.render('account');
 });
+
+
+/*=============================================
+=            Routes for playlists
+=============================================*/
+
+router.get('/create', (req, res, next) => {
+    return res.render('create');
+});
+
+
+/*=============================================
+=            Routes for backend requests
+=============================================*/
 
 router.post('/register', (req, res, next) => {
   request.post({
