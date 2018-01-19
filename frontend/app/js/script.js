a25db5ad283aa7aeaa983339495a7ce2a6cdf1e7
Samantha Carrillo <samanthacarrillo@Samanthas-MacBook-Pro.local>
2018-01-19 10:25:36 -0500

    home page and nav bar links

--- frontend/app/js/script.js
+++ frontend/app/js/script.js
@@ -212,35 +212,4 @@ $(function() {
 /*=============================================
 =            Index Page            =
 =============================================*/
-
-$(function () {
-    // init
-    var controller = new ScrollMagic.Controller({
-        globalSceneOptions: {
-            triggerHook: 'onLeave'
-        }
-    });
-
-    // get all slides
-    var slides = document.querySelectorAll(".panel");
-
-    // create scene for every slide
-    for (var i=0; i<slides.length; i++) {
-        new ScrollMagic.Scene({
-                triggerElement: slides[i]
-            })
-            .setPin(slides[i], {pushFollowers: false})
-            .addTo(controller);
-    }
-
-    // fix navbar when scrolling
-    var banner = $('#nav-container');
-    new ScrollMagic.Scene({
-        triggerElement: '#section-web',
-        triggerHook: 'onCenter',
-        offset: -150
-    })
-        .setClassToggle(banner[0], 'fixed')
-        .setTween(banner[0], 0.3, {top: 0, ease: Power2.EaseIn})
-        .addTo(controller);
-});
+//CHANGE 
